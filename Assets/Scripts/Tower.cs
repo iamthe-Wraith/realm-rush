8a0b81c58fd3633a5b681a6cb07c3151bd03ea64
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-12-31 15:44:54 -0500

    finishing touches

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int buildCost = 50;
+    [SerializeField] float buildTime = 1f;
 
     Bank bank;
+
+    private void Start()
+    {
+        StartCoroutine(Build());    
+    }
 
     public bool CreateTower(Tower tower, Vector3 position)
     {
@@ -17,5 +23,29 @@ public class Tower : MonoBehaviour
         Instantiate(tower.gameObject, position, Quaternion.identity);
         bank.Withdraw(buildCost);
         return true;
+    }
+
+    private IEnumerator Build()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+
+            foreach(Transform grandchild in child)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildTime);
+            
+            foreach(Transform grandchild in child)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
     }
 }
